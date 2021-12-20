9e801cbc67fd732e00acd6461504e52cf0707924
pavellupach <pavellupach@mail.ru>
2021-12-20 17:27:56 +0200

    пути работают

--- my_project/urls.py
+++ my_project/urls.py
@@ -20,4 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
+    path('about', include('posts.urls')),
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,5 +4,9 @@ from .models import Post
 
 class HomePageView(ListView):
     model = Post
-    template_name = 'home.html'
+    template_name = 'my_project/home.html'
     context_object_name = 'all_posts_list'
+
+class AboutPageView(ListView):
+    model = Post
+    template_name = 'my_project/about.html'
